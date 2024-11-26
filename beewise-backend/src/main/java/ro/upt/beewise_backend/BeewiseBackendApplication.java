1e4c73067c30b8239ac15e6cdfd42dfe1ef292af
ptudorr <pop.tudorrr@gmail.com>
2024-11-26 23:48:23 +0200

    initial commit

---
+++ beewise-backend/src/main/java/ro/upt/beewise_backend/BeewiseBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.upt.beewise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeewiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeewiseBackendApplication.class, args);
+	}
+
+}
---
+++ beewise-backend/src/test/java/ro/upt/beewise_backend/BeewiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.upt.beewise_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeewiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
